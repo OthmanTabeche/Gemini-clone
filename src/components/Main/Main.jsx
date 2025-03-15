ef23a3558f5071ae9042006d0f547a85701344d2
Othman Tabeche <otabeche23@gmail.com>
2025-03-15 17:52:02 +0100

    Add: home page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,30 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt="user icon img" />
         </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello Dev.</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming trip</p>
+                    <img src={assets.compass_icon} alt="compas icon img" />
+                </div>
+                <div className="card">
+                    <p>Briefly summaraize this concepts: urban planning</p>
+                    <img src={assets.bulb_icon} alt="compas icon img" />
+                </div>
+                <div className="card">
+                    <p>Brainstrom team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="compas icon img" />
+                </div>
+                <div className="card">
+                    <p>Improve the readibility of the following code</p>
+                    <img src={assets.code_icon} alt="compas icon img" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
