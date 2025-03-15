b2a6a979cebae6915edad288b93d819e255314c6
Othman Tabeche <otabeche23@gmail.com>
2025-03-15 17:03:05 +0100

    Add: nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import react from 'react';
 import SideBar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 const App = () => {
   return (
     <>
       <SideBar />
+      <Main />
     </>
   )
 }
